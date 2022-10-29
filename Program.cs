917ad653658392f5cf4c2cbe0c7e59838e2b860c
Nikolai-Kovalenko <nikolay.kov.al.006@gmail.com>
2022-10-30 00:25:57 +0300

    Функция Drow

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+          int x1 = 13;
+          int y1 = 5;
+          char sym1 = '*';
+          Drow(x1, y1, sym1);
+          Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char sym)
+        {
+         Console.SetCursorPosition(x, y);   
+         Console.Write(sym);               
+        }
+    }
+}
