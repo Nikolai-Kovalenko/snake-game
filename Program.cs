3060a1f64453f0d06c62a249203cc6882dbbc66b
Nikolai-Kovalenko <nikolay.kov.al.006@gmail.com>
2022-10-31 00:49:17 +0200

    Lesons List

--- Program.cs
+++ Program.cs
@@ -8,12 +8,22 @@ namespace snake
         static void Main( string[] args )
         {
 
-      Point p1 = new Point(4, 8, '#');
-         p1.Drow();
+          Point p1 = new Point(4, 8, '#');
+          p1.Drow();
 
-      Point p2 = new Point(9, 18, '&');
-         p2.Drow();
-        Console.ReadLine();
+          Point p2 = new Point(9, 18, '&');
+          p2.Drow();
+          Console.ReadLine();
+
+          List<int> num_list = new List<int>();
+          num_list.Add (0);
+          num_list.Add (4);
+          num_list.Add (6);
+          num_list.Add (17);
+
+          foreach(int i in num_list){
+            Console.WriteLine(i);
+          }
         }
     }
 }
